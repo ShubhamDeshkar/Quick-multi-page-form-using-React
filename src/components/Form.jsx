37f465d6c9d060fca4225167bf8984a0ed81696a
Shubham Deshkar <shubham_deshkar@yahoo.com>
2019-07-04 10:11:23 -0400

    initial commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import FormPersonalDetails from "./FormPersonalDetails";
+import FormUserDetails from "./FormUserDetails";
+
+class Form extends Component {
+	constructor() {
+		super();
+		this.state = {
+			step: 1,
+			firstName: "",
+			lastName: "",
+			email: "",
+			password: "",
+			confirmPassword: "",
+			addressLine1: "",
+			addressLine2: "",
+			city: "",
+			state: "",
+			zipCode: "",
+			phone: "",
+			dob: ""
+		};
+	}
+
+	// next step
+	nextStep = () => {
+		const { step } = this.state;
+		this.setState({
+			step: step + 1
+		});
+	};
+
+	// prev step
+	prevStep = () => {
+		const { step } = this.state;
+		this.setState({
+			step: step - 1
+		});
+	};
+
+	// handle change
+	handleChange = input => e => {
+		this.setState({
+			[input]: e.target.value
+		});
+	};
+
+	render() {
+		const { step } = this.state;
+		const {
+			firstName,
+			lastName,
+			email,
+			password,
+			confirmPassword,
+			addressLine1,
+			addressLine2,
+			city,
+			state,
+			zipCode,
+			phone,
+			dob
+		} = this.state;
+
+		const values = {
+			firstName,
+			lastName,
+			email,
+			password,
+			confirmPassword,
+			addressLine1,
+			addressLine2,
+			city,
+			state,
+			zipCode,
+			phone,
+			dob
+		};
+
+		switch (step) {
+			case 1:
+				return (
+					<FormUserDetails
+						nextStep={this.nextStep}
+						handleChange={this.handleChange}
+						values
+					/>
+				);
+			case 2:
+				return (
+					<FormPersonalDetails
+						nextStep={this.nextStep}
+						prevStep={this.prevStep}
+						handleChange={this.handleChange}
+						values
+					/>
+				);
+			case 3:
+				return <h1>Confirm</h1>;
+			case 4:
+				return <h1>Success!</h1>;
+			default:
+		}
+	}
+}
+
+export default Form;
---
+++ src/components/FormPersonalDetails.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import AppBar from "material-ui/AppBar";
+import TextField from "material-ui/TextField";
+import RaisedButton from "material-ui/RaisedButton";
+
+class FormPersonalDetails extends Component {
+	continue = e => {
+		e.preventDefault();
+		this.props.nextStep();
+	};
+
+	back = e => {
+		e.preventDefault();
+		this.props.prevStep();
+	};
+
+	render() {
+		const { values, handleChange } = this.props;
+		return (
+			<MuiThemeProvider>
+				<React.Fragment>
+					<AppBar title="Enter Address Details" />
+					<TextField
+						hintText="Address Line 1"
+						floatingLabelText="Address Line 1"
+						onChange={handleChange("addressLine1")}
+						defaultValue={values.addressLine1}
+					/>{" "}
+					<br />
+					<TextField
+						hintText="Address Line 2"
+						floatingLabelText="Address Line 2"
+						onChange={handleChange("addressLine2")}
+						defaultValue={values.addressLine2}
+					/>{" "}
+					<br />
+					<TextField
+						hintText="Enter City"
+						floatingLabelText="City"
+						onChange={handleChange("city")}
+						defaultValue={values.city}
+					/>{" "}
+					<br />
+					<TextField
+						hintText="Enter your state"
+						floatingLabelText="State"
+						onChange={handleChange("state")}
+						defaultValue={values.state}
+					/>{" "}
+					<br />
+					<RaisedButton
+						label="Back"
+						// primary={false}
+						style={styles.button}
+						onClick={this.prevStep}
+					/>
+					<RaisedButton
+						label="Continue"
+						primary={true}
+						style={styles.button}
+						onClick={this.continue}
+					/>
+				</React.Fragment>
+			</MuiThemeProvider>
+		);
+	}
+}
+
+const styles = {
+	button: {
+		margin: 15
+	}
+};
+
+export default FormPersonalDetails;
