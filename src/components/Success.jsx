a03f047c236eac2173a1299d1a174fb7dcd42281
Shubham Deshkar <shubham_deshkar@yahoo.com>
2019-07-10 10:33:27 -0400

    change font color to grey

--- src/components/Success.jsx
+++ src/components/Success.jsx
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import AppBar from "material-ui/AppBar";
 import RaisedButton from "material-ui/RaisedButton";
+import { grey, green } from "@material-ui/core/colors";
 
 class FormUserDetails extends Component {
 	startAgain = input => {
@@ -14,12 +15,14 @@ class FormUserDetails extends Component {
 				<React.Fragment>
 					<AppBar title="Thank You!" />
 					<div style={{ marginTop: 30 }}>
-						<h1>
+						<h1 style={{ color: "grey" }}>
 							Thank you for your input. <br />
 							This was just a demo Form created using Material UI <br />
 						</h1>
-						<h2>Don't worry we did not save any information</h2>
-						<a href="www.materialui.com">Check Mterial UI website</a> <br />
+						<h2 style={{ fontWeight: "normal", color: "grey" }}>
+							Don't worry we did not save any information
+						</h2>
+						<a href="www.materialui.com">Check Material UI website</a> <br />
 						<div style={{ marginTop: 30, marginBottom: 20 }}>
 							Check the <code>CODE</code> for this Form on GitHub{" "}
 							<a
